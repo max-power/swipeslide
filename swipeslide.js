65762927d3f3101ed13a7e35010949daf28a5762
DjebbZ <khalid.jebbari@gmail.com>
2011-09-06 18:29:29 +0200

    Added many comments

--- swipeslide.js
+++ swipeslide.js
@@ -1,21 +1,26 @@
 (function($) {
   $.fn.swipeSlide = function(options) {
+    // Merge passed options with defaults
     var options = $.extend({
       content_selector: 'ul', // parent element of the slides
-      vertical: false,   
-      tolerance:0.3,     // values between 0 and 1, where 1 means you have to drag to the center of the slide
-      delay: 0.3,        // animation speed in seconds
+      vertical: false,        // horizontal or vertical
+      tolerance:0.3,          // values between 0 and 1, where 1 means you have to drag to the center of the slide
+      delay: 0.3,             // animation speed in seconds
       bullet_navigation: 'link',   // false, true or link (event handlers will be attached)
-      directional_navigation: false,
-      touch_navigation: true,
-      fancy_cursor: true,
-      threeD: false
+      directional_navigation: false,    // will inset previous and next links
+      touch_navigation: true, // will bind touch events. you don't need this if you don't develop for iOS devices.
+      fancy_cursor: true,     // this you won't see on touch based devises.
+      threeD: false           // 3D Carousel instead of linear slider
     }, options);
 
     return this.each(function(index) {
+      // Initialize variables used across the plugin
       var touch  = {}, current_slide = 0, nav_bullets,
+          // Container of the slides
           self   = $(this).addClass('ui-swipeslide').addClass('ui-swipeslide-' + (options.vertical ? 'vertical' : 'horizontal')).addClass(options.threeD ? 'ui-swipeslide-3d' : ''),
+          // Reference to the current slide
           reel   = self.find(options.content_selector).first().addClass('ui-swipeslide-content'),
+          // Array of all slides must be direct children of the 'content_selector'
           slides = reel.children().addClass('ui-swipeslide-slide'),
           alpha  = 360/slides.length * (options.vertical ? -1 : 1), revolution = radius = 0;
 
@@ -26,12 +31,16 @@
 
       /* bind touch events */
       if (options.touch_navigation) {
+        // Save the position where the user started to touch the current slide
         reel.bind('mousedown touchstart', function(e) {
           /* if (e.touches && e.touches.length > 1) return container.trigger('touchend'); */
           touch.x1 = touch.x2 = e.pageX || e.touches[0].pageX;
           touch.y1 = touch.y2 = e.pageY || e.touches[0].pageY;
           if (!e.touches) e.preventDefault();
-        }).bind('mousemove touchmove', function(e){
+        })
+          // Save the position where the user is moving the current slide, and move it
+          .bind('mousemove touchmove', function(e){
+          // Don't do anything if the mousedown/touchstart events didn't happen
           if (!touch.x1 || !touch.x2) return;
           e.preventDefault();
           touch.x2 = e.pageX || e.touches[0].pageX;
@@ -39,13 +48,16 @@
           var distance = swipe_distance(touch);
           // if (Math.abs(distance) > container_dimension()) { return reel.trigger('touchend'); }
           move_slider(distance);
-        }).bind('mouseup mouseout touchend touchcancel touchleave', function(e) {
+        })
+          // Save the position where the user is releasing the current slide, and move it
+          .bind('mouseup mouseout touchend touchcancel touchleave', function(e) {
           var distance  = swipe_distance(touch);
           var tolerance = options.tolerance * container_dimension() / 2;
           if (Math.abs(distance) > tolerance) {
             current_slide = distance < 0 ? next_slide() : prev_slide();
           }
           move_slider(0, options.delay);
+          // Reinitialize the touch object for the next touch events
           touch = {};
         });
       }
@@ -56,6 +68,11 @@
           .delegate('.first','click', function(){ current_slide = 0; move_slider(0, options.delay); })
           .delegate('.last', 'click', function(){ current_slide = slides.length-1; move_slider(0, options.delay); });
       
+      /**
+       * Initalize the slider
+       *
+       * Set css properties on the slider and the current slide
+       */
       function init() {
         var c = container_dimension();
           reel.css(options.vertical ? 'height' : 'width', c * (options.threeD ? 1 : slides.length)+'px');
@@ -71,12 +88,26 @@
         }
         move_slider();
       }
+      
+      /**
+       * Animate the slides
+       *
+       * @param distance Integer the distance to add to the slides movement in pixels
+       * @param delay Integer the delay in seconds before moving the slides
+       */
       function move_slider(distance, delay) {
         reel.anim(animation_properties(distance || 0), delay || 0);
         slides.removeClass('active').eq(current_slide).addClass('active');
         if (options.fancy_cursor && options.touch_navigation) set_cursor();
         if (options.bullet_navigation) set_active_bullet();
       }
+      
+      /**
+       * Calculate the tridimensionnal amount of movement necessary to apply to the slides
+       *
+       * @param distance Integer the distance to add to the slides movement in pixels
+       * @return Object the css properties to animate and their values
+       */
       function animation_properties(distance) {
         var vectors = [0,0,0];
         if (options.threeD) {
@@ -89,12 +120,31 @@
           return { translate3d: vectors.join(',') }
         }
       }
+      
+      /**
+       * Calculate the container dimension
+       *
+       * @return Integer the container dimension in pixels
+       */
       function container_dimension() {
         return parseInt(self.css(options.vertical ? 'height' : 'width'), 10);
       }
+      
+      /**
+       * Calculate the distance swiped by the user
+       *
+       * @param t Object the touch object that holds the touched positions
+       * @return Integer the swiped distance in pixels
+       */
       function swipe_distance(t) {
         return options.vertical ? (t.y2 - t.y1) : (t.x2 - t.x1);
       }
+      
+      /**
+       * Calculate the index of the previous slide
+       *
+       * @return Integer the index of the previous slide
+       */
       function prev_slide() {
         var p = current_slide-1;
         if (options.threeD) {
@@ -103,6 +153,12 @@
         }
         return Math.max(p, 0);
       }
+      
+      /**
+       * Calculate the index of the next slide
+       *
+       * @return Integer the index of the next slide
+       */
       function next_slide() { 
         var n = current_slide+1;
         if (options.threeD) {
@@ -123,6 +179,7 @@
         }
         reel.css('cursor', c + '-resize');
       }
+      
       /* bullet navigation */
       function build_bullet_navigation() {
         var s = ''; for (i=0; i<slides.length; i++) s+='<li data-index="'+i+'">'+(i+1)+'</li>';
@@ -135,9 +192,11 @@
         }
         self.append(nav_bullets);
       }
+      
       function set_active_bullet() {
         nav_bullets.children('li').removeClass('active').eq(current_slide).addClass('active');
       }
+      
       /* prev/next navigation */
       function build_directional_navigation() {
         self.append('<ul class="ui-swipeslide-nav"><li class="prev">Previous</li><li class="next">Next</li></ul>');
